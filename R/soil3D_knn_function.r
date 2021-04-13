4542831a4805af7b05799a3cac5d78b45ca27204
pejovic <milutinpejovic@gmail.com>
2021-04-13 09:21:18 +0200

    new version

--- R/soil3D_knn_function.r
+++ R/soil3D_knn_function.r
@@ -1,14 +1,14 @@
 
 
-#soil.fun = soil.fun; obs.data = n.obs.data; pred.data = n.pred.data; depth_th = 10; n.obs = n.obs; output = "preparation"
+#soil.fun = soil.fun; obs.data = n.obs.data; pred.data = n.pred.data; depth.th = 10; n.obs = n.obs; output = "preparation"
 
-#' @title soil3D_knn
+#' @title soil3Dknn.pred
 #' @description Perform interpolation of soil variables in 3D by usin knn approach.
 #' @param soil.fun formula object that defines target and predictor variables which will be used in searching for neighboring profiles. (eg. `OC~x + y + DEM + TWI + depth`. In this case, `x`, `y`, `DEM` and `TWI` are included in the searching algorithm. `depth` is mandatory for 3D soil mapping.
 #' @param obs.data  Observation data. Tidy `data.frame` with the following columns: `ID`, `Top`, `Bottom`, `Target variable` column, `Covariates` columns, `x`, `y`, `depth`.
 #' @param pred.data Prediction data. Tidy `data.frame` with the following columns: `ID`, `Covariates` columns, `x`, `y`, `depth`.
 #' @param n.obs number of neighboring profiles.Tuning parameter.
-#' @param depth_th depth treshold. Serves to define prediction depth interval: `pdi = pred.data$depth +/- depth_th`, Tuning parameter.
+#' @param depth.th depth threshold. Serves to define prediction depth interval: `pdi = pred.data$depth +/- depth.th`, Tuning parameter.
 #' @param p power of distance. Tuning parameter. Default: 1
 #' @param output prediction or preparation. If output = "prediction" prediction will be applied, Default: 'prediction'
 #' @return prediction based on IDW and gower distance.
@@ -19,60 +19,72 @@
 #'  #EXAMPLE1
 #'  }
 #' }
-#' @rdname soil3D_knn
-#' @export 
+#' @rdname soil3Dknn.pred
+#' @export
+#' @importFrom magrittr %>% %<>%
 #' @importFrom dplyr filter distinct left_join mutate case_when group_by summarise ungroup arrange
 #' @importFrom gower gower_topn
 #' 
-soil3D_knn <- function(soil.fun, obs.data, pred.data, n.obs, depth_th, p = 1, output = "prediction"){
+#' 
+# soil.fun = xy.edgeroi; obs.data = edgeroi_df[edgeroi_df$ID != "399_EDGEROI_ed003_1", ]; pred.data = edgeroi_df[(edgeroi_df$ID == "399_EDGEROI_ed003_1"), ][6, ]; depth.th = 15; p = 1; folds = edgeroi_folds; params = edgeroi_params; d = 2; n.obs = 5
+
+soil3Dknn.pred <- function(soil.fun, obs.data, pred.data, n.obs, depth.th, p, d, output = "prediction"){
   target.name <- all.vars(soil.fun)[1]
   coord_names <- c("x", "y")
   cov.names <- all.vars(soil.fun)[-1]
   cov.names <- cov.names[-which(cov.names == "depth")]
-  
-  in.obs.data <- obs.data %>% dplyr::filter(depth <= pred.data$depth + depth_th & depth >= pmax(pred.data$depth - depth_th))
+  #TODO: cov.names moraju biti sadrzani u oba seta obs i pred.
+  #TODO: exlude all the observations with the same spatial coordinates as pred.data
+
+  in.obs.data <- obs.data %>% dplyr::filter(depth <= pred.data$depth + depth.th & depth >= pmax(pred.data$depth - depth.th, 0))
   in.obs.data.ID <- unique(in.obs.data$ID)
+  results <- 0
   if(length(in.obs.data.ID) < n.obs){
-    while(length(in.obs.data.ID) < n.obs) {
-      depth_th <- depth_th + 5
-      in.obs.data <- obs.data %>% dplyr::filter(depth <= pred.data$depth + depth_th & depth >= pmax(pred.data$depth - depth_th))
-      in.obs.data.ID <- unique(in.obs.data$ID)
-    }
+    results <- NA
+    #next("There is no enough observations for predicting at this location")
+    # while(length(in.obs.data.ID) < n.obs) {
+    #   depth.th <- depth.th + 2
+    #   in.obs.data <- obs.data %>% dplyr::filter(depth <= pred.data$depth + depth.th & depth >= pmax(pred.data$depth - depth.th, 0))
+    #   in.obs.data.ID <- unique(in.obs.data$ID)
+    # }
   }
-  
-  gower_search <- gower::gower_topn(x = pred.data[, cov.names], y = in.obs.data %>% dplyr::distinct(x, y, .keep_all = TRUE) %>% .[, cov.names], n = n.obs)
-  
-  in.obs.data.ID <- in.obs.data %>% dplyr::distinct(x, y, .keep_all = TRUE) %>% .[as.numeric(gower_search$index), "ID"]
-  in.obs.data <- suppressMessages(in.obs.data %>% dplyr::left_join(data.frame(ID = in.obs.data.ID, gower_distance = gower_search$distance), .))
-  
-  in.obs.data <- in.obs.data %>% dplyr::mutate(pred_top = pmax(pred.data$depth-depth_th, 0), pred_bot = pred.data$depth+depth_th) %>% 
-    dplyr::filter(Top < pred_bot, Bottom > pred_top) %>% 
-    dplyr::mutate(dh = dplyr::case_when(pred_top >= Top & pred_bot <= Bottom ~ pred_bot - pred_top,
-                                 pred_top >= Top & pred_bot >= Bottom ~ Bottom - pred_top,
-                                 pred_top <= Top & pred_bot >= Bottom ~ Bottom - Top,
-                                 pred_top <= Top & pred_bot <= Bottom ~ pred_bot - Top)) %>%
-    dplyr::group_by(ID) %>% dplyr::summarise(obs_mean = weighted.mean(eval(parse(text = target.name)), w = dh/(2*depth_th), na.rm = TRUE),
-                                             gower_distance = mean(gower_distance)) %>%
-    dplyr::ungroup() %>% dplyr::arrange(gower_distance)
-  
-  if(output == "prediction"){
-    results <- in.obs.data %>% dplyr::summarise(pred_mean = sum(obs_mean/gower_distance^p)/sum(1/gower_distance^p)) %>% .$pred_mean
-  }else{
-    results <- in.obs.data
+  if(!is.na(results)){
+    gower_search <- gower::gower_topn(x = pred.data[, cov.names], y = in.obs.data %>% dplyr::distinct(x, y, .keep_all = TRUE) %>% .[, cov.names], n = n.obs)
+    
+    in.obs.data.ID <- in.obs.data %>% dplyr::distinct(x, y, .keep_all = TRUE) %>% .[as.numeric(gower_search$index), "ID"]
+    in.obs.data <- suppressMessages(obs.data %>% dplyr::inner_join(data.frame(ID = in.obs.data.ID, gower_distance = gower_search$distance), .))
+    in.obs.data <- in.obs.data %>% dplyr::mutate(abs_dh = pmax(abs(pred.data$depth - depth), 0.01)) %>% 
+      dplyr::group_by(ID) %>%
+      dplyr::summarise(obs_mean = sum(eval(parse(text = target.name))/abs_dh^d)/sum(1/abs_dh^d),
+                       gower_distance = mean(gower_distance)) %>%
+      dplyr::ungroup() %>% 
+      dplyr::arrange(gower_distance)
+    
+    if(output == "prediction"){
+      results <- in.obs.data %>% dplyr::summarise(pred_mean = sum(obs_mean/gower_distance^p)/sum(1/gower_distance^p)) %>% .$pred_mean
+    }else{
+      results <- in.obs.data
+    }
   }
   return(results)
 }
 
 
 
-#' @title tune_soil3D_knn
-#' @description performs the cross-validation based tuning of meta-parameters.
-#' @param soil.fun formula object that defines target and predictor variables which will be used in searching for neighboring profiles. (eg. `OC~x + y + DEM + TWI + depth`. In this case, `x`, `y`, `DEM` and `TWI` are included in the searching algorithm. `depth` is mandatory for 3D soil mapping.
-#' @param obs.data  Observation data. Tidy `data.frame` with the following columns: `ID`, `Top`, `Bottom`, `Target variable` column, `Covariates` columns, `x`, `y`, `depth`.
+
+#' @title soil3Dknn
+#' @description soil3Dknn interpolation
+#' @param soil.fun soil.fun formula object that defines target and predictor variables which will be used in searching for neighboring profiles. (eg. `OC~x + y + DEM + TWI + depth`. In this case, `x`, `y`, `DEM` and `TWI` are included in the searching algorithm. `depth` is mandatory for 3D soil mapping.
+#' @param obs.data Observation data. Tidy `data.frame` with the following columns: `ID`, `Top`, `Bottom`, `Target variable` column, `Covariates` columns, `x`, `y`, `depth`.
+#' @param pred.data Prediction data. Tidy `data.frame` with the following columns: `ID`, `Covariates` columns, `x`, `y`, `depth`.
 #' @param n.obs number of neighboring profiles.Tuning parameter.
-#' @param params `data.frame` of possible values of parameters (`n.obs`, `depth_th`, `p`).
-#' @param folds vector indicating which profiles belong to each fold.
-#' @return params data.frame with the corresponding accuracy measures
+#' @param depth.th depth threshold. Serves to define prediction depth interval: `pdi = pred.data$depth +/- depth.th`, Tuning parameter, Default: 10
+#' @param p power of horizontal distance. Tuning parameter. Default: 1
+#' @param d power of vertical distance. Tuning parameter. Default: 1
+#' @param output prediction or preparation. If output = "prediction" prediction will be applied, Default: 'prediction'
+#' @param parallel logical, if TRUE the computation will perform in parallel, Default: FALSE
+#' @param n.cores Number of cores for parallel computation, Default: 1
+#' @return prediction based on IDW and gower distance.
 #' @details DETAILS
 #' @examples 
 #' \dontrun{
@@ -80,12 +92,52 @@ soil3D_knn <- function(soil.fun, obs.data, pred.data, n.obs, depth_th, p = 1, ou
 #'  #EXAMPLE1
 #'  }
 #' }
-#' @rdname tune_soil3D_knn
+#' @rdname soil3Dknn
+#' @export
+#' @importFrom doParallel registerDoParallel %dopar%
+#' @importFrom foreach foreach
+#' @importFrom gower gower_topn 
+soil3Dknn <- function(soil.fun, obs.data, pred.data, n.obs, depth.th, p, d, output = "prediction", parallel = FALSE, n.cores = 1){
+  pred.data$prediction <- 0
+  if(parallel){
+    doParallel::registerDoParallel(cores = n.cores)
+    pred.data$prediction <- foreach::foreach(i = 1:dim(pred.data)[1], .combine = c, .packages = "soil3Dknn") %dopar% soil3Dknn::soil3Dknn.pred(soil.fun = soil.fun, obs.data = obs.data, pred.data = pred.data[i, ], depth.th = depth.th, n.obs = n.obs, p = p, d = d)# %>% unlist()
+  }else{
+    for(i in 1:dim(pred.data)[1]){
+      pred.data$prediction[i] <- soil3Dknn.pred(soil.fun = soil.fun, obs.data = obs.data, pred.data = pred.data[i, ], depth.th = depth.th, n.obs = n.obs, p = p, d = d) %>% unlist()
+    }
+  }
+ return(pred.data)
+}    
+
+
+
+
+#' @title tune.soil3Dknn
+#' @description performs the cross-validation based tuning of meta-parameters.
+#' @param soil.fun formula object that defines target and predictor variables which will be used in searching for neighboring profiles. (eg. `OC~x + y + DEM + TWI + depth`. In this case, `x`, `y`, `DEM` and `TWI` are included in the searching algorithm. `depth` is mandatory for 3D soil mapping.
+#' @param obs.data  Observation data. Tidy `data.frame` with the following columns: `ID`, `Top`, `Bottom`, `Target variable` column, `Covariates` columns, `x`, `y`, `depth`.
+#' @param params `data.frame` of possible values of tuning parameters (`depth.th`, `n.obs`, `p`, `d`).
+#' @param folds vector indicating which profiles belong to each fold.
+#' @return `params` data.frame with the corresponding accuracy measures.
+#' @param parallel logical, if TRUE the computation will perform in parallel, Default: FALSE
+#' @param n.cores Number of cores for parallel computation, Default: 1
+#' @details DETAILS
+#' @examples 
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @rdname tune.soil3Dknn
 #' @export 
+#' @importFrom magrittr %>% %<>%
 #' @importFrom dplyr mutate distinct filter group_split
 #' @importFrom tibble rowid_to_column
 #' @importFrom purrr map
-tune_soil3D_knn <- function(soil.fun, obs.data, params, folds){
+#' @importFrom yardstick rsq
+tune.soil3Dknn <- function(soil.fun, obs.data, params, folds, parallel = FALSE, n.cores = 1){
+  "%ni%" <- Negate("%in%")
   params <- params %>% dplyr::mutate(rsq = as.numeric(NA))
   for(k in 1:dim(params)[1]){
     prediction <- data.frame()
@@ -93,24 +145,25 @@ tune_soil3D_knn <- function(soil.fun, obs.data, params, folds){
       train_df_ID <- obs.data %>% dplyr::distinct(x, y, .keep_all = TRUE) %>% .[folds != i, "ID"] # OC mora genericki
       train_df <- obs.data %>% dplyr::filter(ID %in% train_df_ID$ID)
       pred_df <- obs.data %>% dplyr::filter(ID %ni% train_df_ID$ID)
-      pred_list <- pred_df %>% tibble::rowid_to_column() %>% dplyr::group_split(rowid, .keep = FALSE)
-      pred_df$prediction <-   purrr::map(.x = pred_list, .f = ~soil3D_knn(soil.fun = soil.fun, obs.data = train_df, pred.data = .x, depth_th = params$depth_th[k], n.obs = params$n.obs[k], p = params$p[k])) %>% unlist()
+      pred_df <- soil3Dknn::soil3Dknn(soil.fun = soil.fun, obs.data = train_df, pred.data = pred_df, depth.th = params$depth.th[k], n.obs = params$n.obs[k], p = params$p[k], d = params$d[k], parallel = parallel, n.cores = n.cores)
       prediction <- rbind(prediction, pred_df[, c("ID", "Top", "Bottom", all.vars(soil.fun)[1], "prediction")])
     }
-    params[k, "rsq"] <- rsq(prediction, truth = eval(parse(text = all.vars(soil.fun)[1])), estimate = prediction)[3]$.estimate
+    params[k, "rsq"] <- yardstick::rsq(prediction, truth = eval(parse(text = all.vars(soil.fun)[1])), estimate = prediction)[3]$.estimate
   }
   return(params)
 }
 
 
 
-#' @title ncv_soil3D_knn
+#' @title ncv.soil3Dknn
 #' @description performs the nested cross-validation procedure for assessing prediction accuracy.
 #' @param soil.fun formula object that defines target and predictor variables which will be used in searching for neighboring profiles. (eg. `OC~x + y + DEM + TWI + depth`. In this case, `x`, `y`, `DEM` and `TWI` are included in the searching algorithm. `depth` is mandatory for 3D soil mapping.
 #' @param obs.data  Observation data. Tidy `data.frame` with the following columns: `ID`, `Top`, `Bottom`, `Target variable` column, `Covariates` columns, `x`, `y`, `depth`.
 #' @param n.obs number of neighboring profiles.Tuning parameter.
-#' @param params `data.frame` of possible values of parameters (`n.obs`, `depth_th`, `p`).
+#' @param params `data.frame` of possible values of parameters (`depth.th`, `n.obs`, `d`, `p`).
 #' @param folds data.frame with two columns indicating which profiles belong to each fold in outer and inner loop of the nested cross-validation.
+#' @param parallel logical, if TRUE the computation will perform in parallel, Default: FALSE
+#' @param n.cores Number of cores for parallel computation, Default: 1
 #' @return Accuracy measures
 #' @details DETAILS
 #' @examples 
@@ -119,27 +172,31 @@ tune_soil3D_knn <- function(soil.fun, obs.data, params, folds){
 #'  #EXAMPLE1
 #'  }
 #' }
-#' @rdname ncv_soil3D_knn
+#' @rdname ncv.soil3Dknn
 #' @export 
-#' @importFrom dplyr distinct filter group_split
+#' @importFrom dplyr distinct filter group_split bind_rows
+#' @importFrom magrittr %>% %<>%
 #' @importFrom tibble rowid_to_column
 #' @importFrom purrr map
-#' 
-ncv_soil3D_knn <- function(soil.fun, obs.data, params, folds){
+#' @importFrom yardstick rsq
+ncv.soil3Dknn <- function(soil.fun, obs.data, params, folds, parallel = FALSE, n.cores = 1){
+  "%ni%" <- Negate("%in%")
   outer.results <- as.list(rep(NA, length(unique(folds$outer.fold))))
+  best.params <- as.list(rep(NA, length(unique(folds$outer.fold))))
   for(i in 1:length(unique(folds$outer.fold))){
     train_ID <- obs.data %>% dplyr::distinct(x, y, .keep_all = TRUE) %>% .[folds$outer.fold != i, "ID"]
     train_df <- obs.data %>% dplyr::filter(ID %in% train_ID$ID)
     pred_df <- obs.data %>% dplyr::filter(ID %ni% train_ID$ID)
     inner.folds <- folds[folds$outer.fold != i, ] %>% .$inner.fold
-    tune.results <- tune_soil3D_knn(soil.fun = soil.fun, obs.data = train_df, folds = inner.folds, params = params)
+    tune.results <- soil3Dknn::tune.soil3Dknn(soil.fun = soil.fun, obs.data = train_df, folds = inner.folds, params = params, parallel = parallel, n.cores = n.cores)
     best.tune <- tune.results[which.max(tune.results$rsq), 1:3]
-    pred_list <- pred_df %>% tibble::rowid_to_column() %>% dplyr::group_split(rowid, .keep = FALSE)
-    pred_df$prediction <-   purrr::map(.x = pred_list, .f = ~soil3D_knn(soil.fun = soil.fun, obs.data = train_df, pred.data = .x, depth_th = best.tune$depth_th, n.obs = best.tune$n.obs, p = best.tune$p)) %>% unlist()
+    pred_df <- soil3Dknn::soil3Dknn(soil.fun = soil.fun, obs.data = train_df, pred.data = pred_df, depth.th = best.tune$depth.th, n.obs = best.tune$n.obs, p = best.tune$p, d = best.tune$d, output = "prediction", parallel = parallel, n.cores = n.cores)
     outer.results[[i]] <- pred_df[, c("ID", "Top", "Bottom", all.vars(soil.fun)[1], "prediction")]
+    best.params[[i]] <- best.tune
   }
-  outer.rsq <- rsq(outer.results %>% bind_rows(), truth = eval(parse(text = all.vars(soil.fun)[1])), estimate = prediction)[3]$.estimate
-  results <- list(obs.pred = outer.results,  rsq = outer.rsq)
+  outer.rsq <- yardstick::rsq(outer.results %>% dplyr::bind_rows(), truth = eval(parse(text = all.vars(soil.fun)[1])), estimate = prediction)[3]$.estimate
+  outer.rmse <- yardstick::rsq(outer.results %>% dplyr::bind_rows(), truth = eval(parse(text = all.vars(soil.fun)[1])), estimate = prediction)[3]$.estimate
+  results <- list(obs.pred = outer.results, best.params = best.params,  rsq = outer.rsq, rmse = outer.rmse, )
   return(results)
 }
 
